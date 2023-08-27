92ba8698f7b8c6ab833a7bf78d7ec11d393d2fc7
sato3100 <satoru4107@gmail.com>
2023-08-27 17:58:30 +0900

    画面比率変更

--- gacha.java
+++ gacha.java
@@ -9,7 +9,7 @@ public class gacha {
 		//JFrameクラスをインスタンス化
 		JFrame frame = new JFrame("ガチャ");
 		//ウィンドウのサイズを指定(16:9)
-		frame.setSize(1152, 648);
+		frame.setSize(1280, 720);
 		//ウィンドウの位置を、画面の中心へ
 		frame.setLocationRelativeTo(null);
 		//×でアプリが終了するようにする
