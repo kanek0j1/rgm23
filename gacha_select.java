062af386244f9c9b4df8b614731365ade12706cc
sato3100 <satoru4107@gmail.com>
2023-09-03 16:36:03 +0900

    選択画面のファイル

---
+++ gacha_select.java
@@ -0,0 +1,23 @@
+/* さとるコード */
+
+//JFrameクラスのインポート
+import javax.swing.JFrame;
+
+public class gacha_select {
+
+	public static void main(String[] args) {
+		//JFrameクラスをインスタンス化
+		JFrame frame = new JFrame("ガチャ");
+		//ウィンドウのサイズを指定(16:9)
+		frame.setSize(1280, 720);
+		//ウィンドウの位置を、画面の中心へ
+		frame.setLocationRelativeTo(null);
+		//×でアプリが終了するようにする
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//ウィンドウのサイズを変更できないようにする
+		frame.setResizable(false);
+		//ウィンドウを表示する
+		frame.setVisible(true);
+	}
+
+}
